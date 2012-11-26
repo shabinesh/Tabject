57c3e8aa3f78afb7fc291a9846e64302c834a77f
Shabinesh <shabi@fossix.org>
2012-11-26 22:10:29 +0530

    Query execution working

--- orm.py
+++ orm.py
@@ -30,6 +30,7 @@ class MkTableObj:
 
 class Database:
     def __init__(self, database_name, adapter='sqlite3'):
+        self.__dbname__ = database_name
         self._table_cache = {}
         if adapter == 'sqlite3':
             self.adapter = SqliteAdapter(database_name)
@@ -39,33 +40,37 @@ class Database:
 
     def execute(self, q):
         # A adapter should always return a cursor object
-        self.adapter.cursor.execute(q)
+        return self.adapter.execute(q)
 
     def connect(self):
         return self.adapter.connect()
     
     def _mkclass(self, table):
-        print "create,", table
         l = self.adapter.get_columns_and_types(table)
-        obj = type(table, (Orm, ), {'_fields':l.keys()})
+        obj = type(table, (Orm, ), {'_fields' : l.keys(), 'db' : self, '__name__' : table})
         return obj
 
     def __getattr__(self, name):
-        print "get, ", name
         if name in self.__dict__:
             return self.__dict__[name]
         if name in self._table_cache:
             return self._table_cache.get(name)
         t = self._mkclass(name)
-        print "t :", t
         self._table_cache[name] = t
         return t()
         #create the object and add it to _table_cache and return instance of it.
 
+    def __repr__(self):
+        return "<database {0} instance>".format(self.__dbname__)
+        
 class Orm(object):
                
     def filter(self, **kwargs):
-        return self.get_exec(self.__class__.__name__, **kwargs)
+        tree = Tree(self.__name__)
+        tree.parse_and_add(**kwargs)
+        sql = tree.get_sql()
+        print sql
+        return self.db.execute(sql)
 	
     def get(self, **kwargs):
         pass
