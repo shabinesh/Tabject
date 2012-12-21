f6e3a02f25bb25c5b9047a17bb121f3ad4092667
Shabinesh <shabi@fossix.org>
2012-12-21 23:03:38 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "Tabject",
+    version = "0.0.1",
+    author = "Shabinesh Sivaraj",
+    author_email = "shabi@fossix.org",
+    description = ("A lightweight fast python ORM"),
+    license = "BSD",
+    keywords = "ORM",
+#    url = "http://packages.python.org/Tabject",
+)
--- tabject/__init__.py
+++ tabject/__init__.py
@@ -1,3 +1,8 @@
 __author__  = 'Shabinesh'
 __version__ = 0.1
 __email__ = 'shabi@fossix.org'
+
+
+from .orm import *
+from .types import *
+from .adapters import *
